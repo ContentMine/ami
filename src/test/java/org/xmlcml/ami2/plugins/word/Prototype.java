b933f3200ce4df073b8aca3b192333227757974e
Peter Murray-Rust <peter.murray.rust@googlemail.com>
2015-06-02 16:13:19 +0100

    added prototype

---
+++ src/test/java/org/xmlcml/ami2/plugins/word/Prototype.java
@@ -0,0 +1,20 @@
+package org.xmlcml.ami2.plugins.word;
+
+import org.junit.Test;
+import org.xmlcml.ami2.plugins.AMIArgProcessor;
+import org.xmlcml.ami2.plugins.AMIPlugin;
+
+public class Prototype {
+
+	@Test
+	public void testBoWIEEE() {
+		String cmd = "-q ../cmine/target/ieee/musti/Henniger/ -i fulltext.txt --w.words wordFrequencies "
+				+ "--w.stopwords /org/xmlcml/ami2/plugins/word/stopwords.txt "
+				+ "    /org/xmlcml/ami2/plugins/word/clinicaltrials200.txt";		
+		AMIArgProcessor amiArgProcessor = new WordArgProcessor();
+		amiArgProcessor.parseArgs(cmd);
+		amiArgProcessor.runAndOutput();
+		
+	}
+	
+}
